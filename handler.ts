44998d54e8578d7f1a691cdc890e8707ec65c417
naoyoshi <naoyoshi0511@gmail.com>
2018-10-24 17:49:07 +0900

    works

--- handler.ts
+++ handler.ts
@@ -1,18 +1,44 @@
+import { IncomingWebhook } from "@slack/client";
 import { APIGatewayEvent, Callback, Context, Handler } from "aws-lambda";
+import * as moment from "moment-timezone";
 
-export const hello: Handler = (
+moment.tz.setDefault("Asia/Tokyo");
+
+export const onDeparted: Handler = async (
   event: APIGatewayEvent,
   context: Context,
   cb: Callback
 ) => {
+  const webhook = new IncomingWebhook(process.env.SLACK_WEBHOOK_URL);
+
+  const result = await webhook
+    .send({
+      text: generateText(event.body),
+      username: "naoyoshi okamae(bot)"
+    })
+    .catch(cb);
+  if (!result) {
+    return;
+  }
+
   const response = {
     body: JSON.stringify({
       input: event,
-      message:
-        "Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!"
+      message: "posted successfully"
     }),
     statusCode: 200
   };
 
   cb(null, response);
 };
+
+function generateText(body: string): string {
+  const minutesToDestination = JSON.parse(body).minutesToDestination as number;
+  const arrivesAt = moment().add(minutesToDestination, "m");
+  const hourText =
+    arrivesAt.minutes() < 30
+      ? `${arrivesAt.hour()}時過ぎ`
+      : `${arrivesAt.hour() + 1}時前`;
+
+  return `${hourText}に着きます`;
+}
